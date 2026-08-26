54facc78c80531d1c4b4e73aafb4e995c8cdd6d0
Mridul inturu <mrinturu07@gmail.com>
2026-08-26 14:38:51 +0530

    fix cors for local frontend

--- frontend/src/components/StockSelector.jsx
+++ frontend/src/components/StockSelector.jsx
@@ -90,20 +90,9 @@ function StockSelector({ selectedSymbol, onSelect }) {
 
       <div className="stock-search-wrapper">
         <div className="stock-search">
-          <svg
-            width="17"
-            height="17"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 15.4183 19 11 19 6.58172 15.4183 3 11 3 6.58172 6.58172 3 11 3 15.4183 6.58172 19 11 19Z"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-          </svg>
+          <span className="search-icon" aria-hidden="true">
+            ⌕
+          </span>
 
           <input
             type="text"
